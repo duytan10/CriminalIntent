b76344be5c4f2eb40b89e4c119596baa5c2d1ed5
DuyTan <nhoxbybyby142133@gmail.com>
2021-06-09 17:23:30 +0700

    Chapter 7 to chapter 10

---
+++ app/src/main/java/com/example/criminalintent/Crime.java
@@ -0,0 +1,44 @@
+package com.example.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        this.mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        this.mSolved = solved;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        this.mTitle = title;
+    }
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+}
